9c85d20de35492ed884499ef7b96b85e13a36148
Lois Choi <loisch22@gmail.com>
2017-07-07 15:46:27 -0700

    add empty options, add else if and else to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
-  $("#submit").click(function(){
-    var epiQ1 = $("#epicodusQ1").val();
-    var epiQ2 = $("#epicodusQ2").val();
-    var epiQ3 = $("#epicodusQ3").val();
-    var epiQ4 = $("#epicodusQ4").val();
-    var epiQ5 = $("#epicodusQ5").val();
+  $("#submit").click(function(event){
+    event.preventDefault();
+
     var rubyRQ1 = $("#rubyRailsQ1").val();
     var rubyRQ2 = $("#rubyRailsQ2").val();
     var phpDQ1 = $("#phpDrupalQ1").val();
@@ -15,126 +12,116 @@ $(document).ready(function(){
     var cssDQ2 = $("#cssDesignQ2").val();
     var cNtQ1 = $("#cNetQ1").val();
     var cNtQ2 = $("#cNetQ2").val();
+    var locationInput = $("#location").val();
 
-    var epicodusResult = 0;
     var rubyRailsResult = 0;
     var phpDrupalResult = 0;
     var javaAndResult = 0;
     var cssDesignResult = 0;
     var cNetResult = 0;
 
-    if (epiQ1 === "Yes"){
-      epicodusResult += 1;
-    } else {
-      epicodusResult = 0;
-    }
-    if (epiQ2 === "Yes"){
-      epicodusResult += 1;
-    } else {
-      epicodusResult = 0;
-    }
-    if (epiQ3 === "Yes"){
-      epicodusResult += 1;
-    } else {
-      epicodusResult = 0;
-    }
-    if (epiQ4 === "Yes"){
-      epicodusResult += 1;
-    } else {
-      epicodusResult = 0;
-    }
-    if (epiQ5 === "Yes"){
-      epicodusResult += 1;
-    } else {
-      epicodusResult = 0;
-    }
-
     if (rubyRQ1 === "Yes"){
       rubyRailsResult += 1;
+    } else if (rubyRQ1 === "No"){
+      rubyRailsResult = 0;
     } else {
       rubyRailsResult = 0;
     }
+
     if (rubyRQ2 === "Yes"){
       rubyRailsResult += 1;
+    } else if (rubyRQ2 === "No"){
+      rubyRailsResult = 0;
     } else {
       rubyRailsResult = 0;
     }
 
     if (phpDQ1 === "Yes"){
       phpDrupalResult += 1;
+    } else if (phpDQ1 === "No"){
+      phpDrupalResult = 0;
     } else {
       phpDrupalResult = 0;
     }
+
     if (phpDQ2 === "Yes"){
       phpDrupalResult += 1;
+    } else if (phpDQ2 === "No"){
+      phpDrupalResult = 0;
     } else {
       phpDrupalResult = 0;
     }
 
     if (javaAQ1 === "Yes"){
       javaAndResult += 1;
+    } else if (javaAQ1 === "No"){
+      javaAndResult = 0;
     } else {
       javaAndResult = 0;
     }
+
     if (javaAQ2 === "Yes"){
       javaAndResult += 1;
+    } else if (javaAQ2 === "No"){
+      javaAndResult = 0;
     } else {
       javaAndResult = 0;
     }
 
     if (cssDQ1 === "Yes"){
       cssDesignResult += 1;
+    } else if (cssDQ1 === "No"){
+      cssDesignResult = 0;
     } else {
       cssDesignResult = 0;
     }
+
     if (cssDQ2 === "Yes"){
       cssDesignResult += 1;
+    } else if (cssDQ2 === "No"){
+      cssDesignResult = 0;
     } else {
       cssDesignResult = 0;
     }
 
     if (cNtQ1 === "Yes"){
       cNetResult += 1;
-    } else {
+    } else if (cNtQ1 === "No"){
       cNetResult = 0;
-    }
-    if (cNtQ2 === "Yes"){
-      cNetResult += 1;
     } else {
       cNetResult = 0;
     }
 
-    if (epicodusResult >= 4) {
-      alert("Looks like Epicodus is a great fit for you!");
-      $(".epicodusY").show();
-      $("#contact").show();
+    if (cNtQ2 === "Yes"){
+      cNetResult += 1;
+    } else if (cNtQ1 === "No"){
+      cNetResult = 0;
     } else {
-      alert("Oh no! Looks like Epicodus is not the best for you.");
-      $(".epicodusN").show();
+      cNetResult = 0;
     }
 
     if (rubyRailsResult = 2) {
-      $(".rubyRailsShow").show();
+      $("#rubyRailsShow").show();
     } else {
-      $(".rubyRailsShow").hide();
+      $("#rubyRailsShow").hide();
     }
 
     if (phpDrupalResult = 2) {
-      $(".phpDrupalShow").show();
+      $("#phpDrupalShow").show();
     } else {
-      $(".phpDrupalShow").hide();
+      $("#phpDrupalShow").hide();
     }
 
     if (javaAndResult = 2) {
-      $(".javaAndShow").show();
+      $("#javaAndShow").show();
     } else {
-      $(".javaAndShow").hide();
+      $("#javaAndShow").hide();
     }
 
     if (cssDesignResult = 2) {
-      $(".cssDesignShow").show();
+      $("#cssDesignShow").show();
     } else {
-      $(".cssDesignShow").hide();
+      $("#cssDesignShow").hide();
     }
 
     if (cNetResult = 2) {
